81e2469fcfcd01e73456d8e4773b33bd1ad8ce89
K7 <k7atwork@users.noreply.github.com>
2016-07-20 10:02:22 +0930

    Javascript to refresh CAPTCHA

---
+++ captchaTimerRefresh.js
@@ -0,0 +1,17 @@
+#captcha
+
+<script type="text/javascript">
+
+function captchaRefresh()
+{
+ submitAction_win0(document.win0,'G_FORM_TTLE_WRK_REFRESH_ICN');
+}
+
+function captchaTimer()
+{
+ setTimeout(function() {captchaRefresh();}, 5000);
+}
+
+window.onload = captchaTimer();
+
+</script>
